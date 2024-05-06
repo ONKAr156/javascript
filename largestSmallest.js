acf205060148f337abaa8914f108dc31419b60b6
ONKAr156 <borgaonkar1998@gmail.com>
2024-05-06 16:47:25 +0530

    JS practice

---
+++ largestSmallest.js
@@ -0,0 +1,32 @@
+const arr = [1000, 2500, 22, 20, 16555, 45, 30, 1500, 1]
+
+
+const findLargest = (arr) => {
+    let largest = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (largest < arr[i]) {
+            largest = arr[i]
+        } else {
+            largest[0]
+        }
+    }
+    return console.log(largest);
+}
+
+findLargest(arr)
+
+
+const smallestNum = (arr) => {
+    let smallest = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < smallest) {
+            smallest = arr[i]
+        } else {
+            smallest[0]
+        }
+
+    }
+    return console.log(smallest);
+}
+
+smallestNum(arr)
