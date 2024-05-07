fcd25ac6e657ed3782cb487916b944dfacfcea1f
ONKAr156 <borgaonkar1998@gmail.com>
2024-05-07 17:31:49 +0530

    Updated

---
+++ sortArray.js
@@ -0,0 +1,20 @@
+const arr = [10, 5, 47, 56, 1, 3, 7, 88, 5, 9];
+
+const bubbleSort = (arr) => {
+    const n = arr.length;
+    for (let i = n; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1] imp 👇
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]; 
+            }
+        }
+    }
+    return console.log(arr) 
+}
+
+bubbleSort(arr)
+
+// ---------------------------------------------------------------------------------------
+
+
