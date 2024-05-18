375da94ef426644b0e9ea561162ce5fe9d082cdd
ONKAr156 <borgaonkar1998@gmail.com>
2024-05-18 11:48:48 +0530

    Updated

---
+++ insertionSort.js
@@ -0,0 +1,21 @@
+const arr = [7, 2, 4, 6, 5, 1, 3]
+
+const insertionSort = (arr) => {
+    for (let i = 1; i < arr.length; i++) {
+        for (let j = i - 1; j >= 0; j--) {
+            if (arr[i] < arr[j]) {
+                console.log(arr[i]);
+                console.log(arr[j]);
+                arr[j] = arr[i]
+            }
+
+        }
+
+    }
+    return console.log(arr);
+
+
+}
+
+
+insertionSort(arr)
---
+++ recursion.js
@@ -0,0 +1,60 @@
+// # print reverse number from 10
+// const test = (n) => {
+//     if (n < 1) {
+//         return
+//     }
+
+//     console.log(n);
+//     test(n - 1)
+// }
+
+// test(10)
+// 0+1 
+//1,2,3,5,8,13,21
+
+
+// # Print the Fibonacci series till 10
+// let fibo = [0, 1]
+// for (let i = 2; i < 10; i++) {
+//     fibo.push(fibo[i - 1] + fibo[i - 2])
+// }
+// console.log(fibo)
+
+//# IMP CHECK USING DEBBUGER
+// const sum = (arg) => {
+//     if (arg === 0) {
+//         return 0
+//     }
+//     return arg + sum(arg - 1)
+
+// }
+
+// const result = sum(5)
+// console.log(result);
+
+
+//# Reverse the string
+const str = "HELLO";
+console.log(str.substring(1));
+const revStr = (arg) => {
+    if (arg.length === 0) {
+        return arg;
+    }
+    return revStr(arg.substring(1)) + arg.charAt(0);
+
+    /*
+    hello
+     ello +  h
+     llo +  e
+     lo + l
+     o + l
+    ""+ o
+     last  olleh
+
+    */
+}
+
+console.log(revStr(str));
+
+
+
